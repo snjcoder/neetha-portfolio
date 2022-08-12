095d47176350e534b7ad6633fbc67104225b9a1b
Neetha <neethacohort@gmail.com>
2022-08-12 01:43:37 -0600

    added contact us

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home/Home'
 import About from './components/About/About'
-// import Contact from './components/Contact'
+import Contact from './components/Contact/Contact'
 import Layout from './components/Layouts/Layout'
 // import Portfolio from './components/Portfolio'
 // import Dashboard from './components/Dashboard'
 import './App.scss'
+
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          {/* <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/contact" element={<Contact />} />
           {/* <Route path="/portfolio" element={<Portfolio />} /> */}
           {/* <Route path="/dashboard" element={<Dashboard />} /> */}
         </Route>
